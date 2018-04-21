e1bb8c0fa59fa9cf6fa4eb1ed31843d8c4d764c8
jorgenario <jmnario@bu.edu>
2018-04-21 12:46:23 -0400

    ips

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const ip = require('ip')
+const iploc = require('iplocation')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(ip.address()))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+// app.get('/', (req, res) => res.send(ip.address()))
