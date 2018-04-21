a1fa2ee2d98afc92981f45e73217649d17a13acc
Warren-Partridge <maps@perdix.org>
2018-04-21 18:56:56 -0400

    Add frontend template

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ app.engine('hbs', hbs);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
     res.render('index');
