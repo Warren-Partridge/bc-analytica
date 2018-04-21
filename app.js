49c5ee429a33111e36f0899a7133f5d3b590c600
Michael Hendrick <MichaelHendrick@Michaels-MacBook-Pro-2.local>
2018-04-21 14:12:38 -0400

    handlebars added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // For getting ip info
 const ip = require('ip');
@@ -8,6 +9,15 @@ const iploc = require('iplocation');
 // For getting device and browser info
 const useragent = require('useragent');
 const si = require('systeminformation');
+
+// view engine setup
+var hbs = require('express-handlebars')({
+  extname: '.hbs'
+});
+app.engine('hbs', hbs);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 
 app.get('/', (req, res) => {
     res.send(ip.address());
@@ -18,6 +28,13 @@ app.get('/', (req, res) => {
     si.osInfo()
         .then(data => console.log(data))
         .catch(error => console.error(error)); // TODO: hostname: 'warrens-air.bc.edu' ... we may be able to get first name from apple users by looking at hostname
+});
+
+app.get('/test', (req, res) => {
+  const apples = ({
+    data: "this is the data"
+  });
+  res.render('testing', {apples: apples});
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
