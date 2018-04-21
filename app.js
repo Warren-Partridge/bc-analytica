9c1681f741123f485e8d3bc0da92aaad8b10e2af
Michael Hendrick <MichaelHendrick@michaels-mbp-2.bc.edu>
2018-04-21 16:38:24 -0400

    Facebook Auth Stuff

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
 
 
 // For getting ip info
@@ -18,6 +20,41 @@ var hbs = require('express-handlebars')({
 app.engine('hbs', hbs);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+
+passport.use(new Strategy({
+    clientID: "359392861234682",
+    clientSecret: "7f1e095cf413e5ad62993edaa57e83c7",
+    callbackURL: "http://localhost:3000/login/facebook/return"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // In this example, the user's Facebook profile is supplied as the user
+    // record.  In a production-quality application, the Facebook profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    return cb(null, profile);
+  }));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/login/facebook/return',
+  passport.authenticate('facebook', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 
 app.get('/test', (req, res) => {
