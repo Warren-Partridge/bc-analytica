edcc9e42a83088ef0efc561b5220e27b33eea766
Warren-Partridge <maps@perdix.org>
2018-04-21 14:57:50 -0400

    Show useragent info in index page

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/', (req, res) => res.send("social-media.js"));
-
 
 // For getting ip info
 const ip = require('ip');
@@ -22,22 +20,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 
-app.get('/', (req, res) => {
-    res.send(ip.address());
-    var agent = useragent.parse(req.headers['user-agent']);
-    // console.log(agent.toJSON());
-    console.log("Welcome, " + agent.toJSON().family + " user");
-
+app.get('/test', (req, res) => {
     si.osInfo()
         .then(data => console.log(data))
         .catch(error => console.error(error)); // TODO: hostname: 'warrens-air.bc.edu' ... we may be able to get first name from apple users by looking at hostname
 });
 
-app.get('/test', (req, res) => {
-  const apples = ({
-    data: "this is the data"
-  });
-  res.render('testing', {apples: apples});
+app.get('/', (req, res) => {
+    var agent = useragent.parse(req.headers['user-agent']);
+
+    const userData = ({
+        ipAddress: ip.address(),
+        browserFamily: agent.toJSON().family,
+        osInfo: "TODO: Get asynchonously"
+    });
+    res.render('index', {userData: userData});
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
