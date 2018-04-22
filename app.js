99c48c5dd20994ac9999ddccfd353562910bd840
jorgenario <jmnario@bu.edu>
2018-04-21 21:51:53 -0400

    Config for GCP, and finally added node start

--- app.js
+++ app.js
@@ -63,4 +63,4 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(8080, () => console.log('Example app listening on port 8080!'));
